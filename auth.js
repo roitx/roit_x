02868d2a36bd6983729dad02971102433cee6db0
roitx <desicreator9199@gmail.com>
2025-12-09 14:21:15 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,3 +1,28 @@
-async function checkLogin(){ const res = await supabase.auth.getUser(); const user = res.data?.user; if(!user) return false; return user; }
-async function ensureLogin(){ const u = await checkLogin(); if(!u) location.href='index.html'; }
-async function doLogout(){ await supabase.auth.signOut(); location.href='index.html'; }
+async function signUpUser() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { error } = await window.supabaseClient.auth.signUp({
+    email,
+    password
+  });
+
+  if (error) alert(error.message);
+  else {
+    alert("Signup successful! Login now.");
+    window.location.href = "index.html";
+  }
+}
+
+async function loginUser() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { error } = await window.supabaseClient.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) alert(error.message);
+  else window.location.href = "dashboard.html";
+}
